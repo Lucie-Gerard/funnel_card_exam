a7da3ece699aaf3c8088ddd14df2c971382be77c
Lucie Gérard <tech.gerardl@gmail.com>
2024-06-21 15:18:59 +0200

    Réajustement des couleurs de fond (backgrounds)

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,11 +9,12 @@ export default {
     extend: {
       colors: {
         // Backgrounds colors
-        bgPrimary: 'rgb(200, 200, 253)',
-        bgSecondary: 'rgb(248, 249, 253)',
+        bgPrimary: 'rgb(213, 235, 255)',
+        bgSecondary: 'rgb(255, 253, 253)',
 
-        // Flaschards
-        fcColor: 'rgb(251, 242, 247)',
+        // Flaschards and inputs background
+        fcColor: 'rgb(246, 251, 255)',
+        bgInput2: 'rgb(246, 251, 255)',
 
         // Text color
         txtColor: 'rgb(0, 10, 12)',
