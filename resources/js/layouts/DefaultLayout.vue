51b42852b2e3c31c55a498f4e748b04434ea0b78
Lucie Gérard <tech.gerardl@gmail.com>
2024-06-23 15:25:03 +0200

    flex et grid pour affichage responsive

--- resources/js/layouts/DefaultLayout.vue
+++ resources/js/layouts/DefaultLayout.vue
@@ -8,13 +8,15 @@ components: [
 </script>
 
 <template>
-    <div class="bg-bgSecondary container mx-auto h-screen 
+    <div class="bg-bgSecondary container mx-auto 
                 border-2 rounded-sm border-bgSecondary 
                 pb-5 my-4">
 
         <DefaultHeader></DefaultHeader>
 
-        <main class="px-8">
+        <main class="px-2
+                     sm:px-4
+                     md:px-8">
             <RouterView></RouterView>
         </main>
     </div>
