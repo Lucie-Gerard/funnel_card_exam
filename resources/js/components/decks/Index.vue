a67e0fed94f37fd4ef72155bd5ee8c58e28aeed4
Lucie Gérard <tech.gerardl@gmail.com>
2024-06-26 16:10:48 +0200

    Input search deck fonctionnel
    Utilisation de defineProps et defineEmits dans le composant Input pour envoyer les données saisies vers le v-model sur le composant Input de la vue Index.
    
    Récupération de la valeur de l'input dans une variable réactive.
    
    Utilisation de computed pour filtrer l'array decks avec cette valeur. Si pas de valeur, retourne toutes les valeurs de decks.

--- resources/js/components/decks/Index.vue
+++ resources/js/components/decks/Index.vue
@@ -1,7 +1,7 @@
 <script setup>
 import Input from '../modules/Input.vue';
 import axios from 'axios';
-import { onMounted, ref } from 'vue';
+import { computed, onMounted, ref } from 'vue';
 
 components: {
     Input
@@ -28,6 +28,16 @@ const deleteDeck = (id, index)  => {
             console.log(error)
         });
 }
+
+
+const filterDeck = ref('');
+
+const filteredDeck = computed(() => {
+    let filter = filterDeck.value
+
+    if (!filter.length) return decks.value;
+    return decks.value.filter(deck => deck.name.toLowerCase().includes(filter));
+})
 </script>
 
 <template>
@@ -35,7 +45,7 @@ const deleteDeck = (id, index)  => {
         <div class="flex items-center justify-start">
             <h2 class="mr-4 sm:mr-8
                        text-xs sm:text-sm md:text-lg">
-                       Your decks
+                       Your decks {{ filterDeck }}
             </h2>
 
             <router-link :to="{ name: 'createDeck' }">
@@ -45,10 +55,11 @@ const deleteDeck = (id, index)  => {
         </div>
 
         <form class="flex items-center justify-end relative">
-            <Input placeholder="Search for a deck"
-                    class="mr-2 sm:mr-4
-                           sm:pl-2
-                           pr-6 sm:pr-8" />
+            <Input v-model="filterDeck"
+                   placeholder="Search for a deck"
+                   class="mr-2 sm:mr-4
+                          sm:pl-2
+                          pr-6 sm:pr-8" />
             <img src="../../../../public/assets/img/deck/search-icon.png" alt="search for a deck"
                 class="size-2 sm:size-4 absolute right-4 sm:right-6">
         </form>
@@ -60,49 +71,37 @@ const deleteDeck = (id, index)  => {
         Build your first deck
     </p>
 
-    <section class="grid grid-cols-2 gap-x-2
-                    sp:grid-cols-3 sm:gap-x-2
-                    sm:grid-cols-4 sm:gap-x-4 
-                    md:gap-x-6
-                    lg:grid-cols-6
-                    xl:grid-cols-8 xl:gap-x-8">
-        <div class="flex flex-col
-                    relative
-                    place-items-center
-                    w-20 min-h-40
-                    sm:w-28 sm:min-h-48
-                    md:w-36 md:min-h-56 
-                    mb-8 
+    <section class="grid grid-cols-2 sp:grid-cols-3 sm:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8
+                    gap-x-2 sm:gap-x-4 md:gap-x-6 xl:gap-x-8">                     
+        <div class="flex flex-col place-items-center relative     
+                    w-20 sm:w-28 md:w-36 
+                    min-h-40 sm:min-h-48 md:min-h-56
+                    mb-4 sm:mb-8 
                     border-2 border-stroke rounded-sm"
-             v-for="(deck, index) in decks" :key="deck.id">
+             v-for="(deck, index) in filteredDeck" :key="deck.id">
 
-            <h3 class="mt-2 mx-4 max-h-10
+            <h3 class="mt-2 sm:mt-4 mx-4 max-h-10 md:max-h-36
                        overflow-hidden
-                       text-xs
-                       sm:text-sm sm:mt-4
-                       md:text-base md:max-h-36">
+                       text-xs sm:text-sm md:text-base">
                 {{ deck.name }}
             </h3>
                 
-            <hr class="absolute border-1 border-stroke mb-4 w-0
-                       md:w-28 bottom-32">
+            <hr class="absolute border-1 border-stroke mb-4 w-0 md:w-28 bottom-32">
+                      
             <div class="absolute bottom-4">
                 <router-link :to="{ name: 'cards', params: { id: deck.id } }">
                     <img src="../../../../public/assets/img/deck/edit.png" alt="edit deck's name and display cards"
-                        class="size-4 mb-4
-                               sm:size-6">
+                        class="size-4 sm:size-6 mb-4">
                 </router-link>
 
                 <router-link :to="{ name: 'playCards', params: { id: deck.id } }">
                     <img src="../../../../public/assets/img/deck/play.png" alt="play cards"
-                        class="size-4 mb-4
-                               sm:size-6">
+                        class="size-4 sm:size-6 mb-4">
                 </router-link>
 
                 <button @click="deleteDeck(deck.id, index)">
                     <img src="../../../../public/assets/img/deck/delete.png" alt="delete deck"
-                        class="size-4
-                               sm:size-6">
+                        class="size-4 sm:size-6">   
                 </button>
             </div>
         </div>
--- resources/js/components/modules/Input.vue
+++ resources/js/components/modules/Input.vue
@@ -1,9 +1,12 @@
 <script setup>
+const props = defineProps(['modelValue']);
 
-
+const emit = defineEmits(['update:modelValue']);
 </script>
 
 <template>
     <input type="text" class="border-2 border-stroke rounded-sm
-                              text-xs sm:text-sm md:text-base">
+                              text-xs sm:text-sm md:text-base"
+           :value="modelValue"
+           @input="$emit('update:modelValue', $event.target.value.toLowerCase())">
 </template>
