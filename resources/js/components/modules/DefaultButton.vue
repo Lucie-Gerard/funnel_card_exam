71ad1638eb415cbfb075759e49363838401c27e2
Lucie Gérard <tech.gerardl@gmail.com>
2024-06-26 12:19:10 +0200

    Composant DefaultButton responsive

--- resources/js/components/modules/DefaultButton.vue
+++ resources/js/components/modules/DefaultButton.vue
@@ -1,7 +1,9 @@
 <script></script>
 
 <template>
-    <button class="border-1 rounded-md bg-bgInput2 text-primary font-bold py-1 px-4 shadow-buttonShadow">
+    <button class="border-1 rounded-md bg-bgInput2 text-primary font-bold py-1 shadow-buttonShadow
+                   px-2 sm:px-4
+                   text-xs sm:text-sm md:text-lg">
         <slot/>
     </button>
 </template>
